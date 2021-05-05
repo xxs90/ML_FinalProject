7e4b24e6b7e97c328fc5e48a6a670231afdf7063
Yuwei <47401847+wyuwei416@users.noreply.github.com>
2021-05-04 22:41:39 -0400

    Update LR_Digit_Recognition.py

--- LR_Digit_Recognition.py
+++ LR_Digit_Recognition.py
@@ -6,7 +6,7 @@
 
 import numpy as np
 import pandas as pd
-# from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from scipy import optimize as opt
 
 # Load Data
@@ -34,6 +34,22 @@ x = np.hstack((bias_train, x))
 bias_test = np.ones((TEST_SIZE, 1))
 test_data = np.hstack((bias_test, test_data))
 
+def initialize(num_inputs,num_classes):
+    w = np.random.randn(num_classes, num_inputs) / np.sqrt(num_classes*num_inputs)
+    b = np.random.randn(num_classes, 1) / np.sqrt(num_classes)
+    
+    param = {
+        'w' : w,
+        'b' : b
+    }
+    return param
+
+def softmax(z):
+    exp_list = np.exp(z)
+    result = 1/sum(exp_list) * exp_list
+    result = result.reshape((len(z),1))
+    assert (result.shape == (len(z),1))
+    return result
 
 def sigmoid(Z):
     return 1.0 / (1 + np.exp(-Z))
@@ -50,6 +66,8 @@ def logistic_gradient(x, y, theta):
     gradient = x.T.dot(H - y) / y.size
     return gradient
 
+def logistic_loss(pred, label):
+    return -np.log(pred[int(label)])
 
 def class_classifier(x, y):
     # This method trains a single classifier per class. Each sample of that class
@@ -77,6 +95,107 @@ def classify(x, W):
     class_name = np.argmax(W.dot(x.T).T, axis=1)
     return class_name
 
+def eval(param, x_data, y_data):
+    loss_list = []
+    w = param['w'].transpose()
+    dist = np.array([np.squeeze(softmax(np.matmul(x_data[i], w))) for i in range(len(y_data))])
+
+    result = np.argmax(dist,axis=1)
+    accuracy = np.sum(result == y_data)/float(len(y_data))
+    
+
+    loss_list = [logistic_loss(dist[i],y_data[i]) for i in range(len(y_data))]
+    loss = sum(loss_list)
+    return loss, accuracy
+
+def mini_batch_gradient(param, x_batch, y_batch):
+    batch_size = x_batch.shape[0]
+    w_grad_list = []
+    b_grad_list = []
+    batch_loss = 0
+    for i in range(batch_size):
+        x,y = x_batch[i],y_batch[i]
+        x = x.reshape((785,1))
+        E = np.zeros((10,1)) #(10*1)
+        E[y][0] = 1 
+        pred = softmax(np.matmul(param['w'], x)+param['b'])
+
+        loss = logistic_loss(pred, y)
+        batch_loss += loss
+
+        w_grad = E - pred
+        w_grad = - np.matmul(w_grad, x.reshape((1,785)))
+        w_grad_list.append(w_grad)
+
+        b_grad = -(E - pred)
+        b_grad_list.append(b_grad)
+
+    dw = sum(w_grad_list)/batch_size
+    db = sum(b_grad_list)/batch_size
+    return dw, db, batch_loss
+
+def train(param, x_train, y_train, x_test, y_test):
+    num_epoches = 100
+    batch_size = 64
+    learning_rate = 0.0025
+    mu = 0.9
+    test_loss_list, test_accu_list = [],[]
+
+    for epoch in range(num_epoches):
+        
+        # select the random sequence of training set
+        rand_indices = np.random.choice(x_train.shape[0],x_train.shape[0],replace=False)
+        num_batch = int(x_train.shape[0]/batch_size)
+        batch_loss100 = 0
+        
+        # for each batch of train data
+        for batch in range(num_batch):
+            index = rand_indices[batch_size*batch:batch_size*(batch+1)]
+            x_batch = x_train[index]
+            y_batch = y_train[index]
+
+            # calculate the gradient w.r.t w and b
+            dw, db, batch_loss = mini_batch_gradient(param, x_batch, y_batch)
+            batch_loss100 += batch_loss
+            param['w'] -= learning_rate * dw
+            param['b'] -= learning_rate * db
+            if batch % 100 == 0:
+                message = 'Epoch %d, Batch %d, Loss %.2f' % (epoch+1, batch, batch_loss)
+                print(message)
+
+                batch_loss100 = 0
+        train_loss, train_accu = eval(param,x_train,y_train)
+        test_loss, test_accu = eval(param,x_test,y_test)
+        test_loss_list.append(test_loss)
+        test_accu_list.append(test_accu)
+
+        message = 'Epoch %d, Train Loss %.2f, Train Accu %.4f, Test Loss %.2f, Test Accu %.4f' % (epoch+1, train_loss, train_accu, test_loss, test_accu)
+        print(message)
+    return test_loss_list, test_accu_list
+
+
+
+def plot(loss_list, accu_list):
+    """store the plots"""
+    # epoch_list = list(range(len(loss_list)))
+    plt.plot(loss_list)
+    plt.ylabel('Loss Function')
+    plt.xlabel('Epoch')
+    plt.xticks(rotation=60)
+    plt.title('Loss Function ~ Epoch')
+    plt.savefig('loss.png')
+    plt.show()
+
+    plt.plot(accu_list)
+    plt.ylabel('Test Accuracy')
+    plt.xlabel('Epoch')
+    plt.xticks(rotation=60)
+    plt.title('Test Accuracy ~ Epoch')
+    plt.savefig('accr.png')
+    plt.show()
+
+
+
 
 # The training data is split in to training set and validation set.
 N = TRAIN_SIZE - VALIDATION_SIZE
@@ -96,4 +215,12 @@ prediction = classify(test_data, W)
 
 # Save the predictions for submission.
 submission = pd.DataFrame(prediction, index=test.index, columns=['Label'])
+
+param = initialize(x.shape[1],len(set(y)))
+# train the model
+loss_list, accu_list = train(param,X_train,Y_train,X_valid,Y_valid)
+
+# plot the loss and accuracy
+plot(loss_list, accu_list)
+
 submission.to_csv("logistic_regression_submission.csv")
